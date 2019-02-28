13c9e77e1b7c1c4f2276d8dfb6f25cfa20149d26
MD. Noor Uddin <nooruddinneon16321@gmail.com>
2019-02-28 15:51:19 +0600

    How Many Ways?

---
+++ How_Many_ways.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    int n,m,I=1;
+    while(t--)
+    {
+        cin>>n>>m;
+        int u,v;
+        m++;
+        while(m--)cin>>u>>v;
+        cout<<"Case "<<I++<<": "<<1<<endl;
+    }
+
+}
